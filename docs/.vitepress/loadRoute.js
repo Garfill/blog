b9e480324209bf56426ff1464856cbca982660ec
Garfill <junhui7guan@163.com>
2024-07-18 22:13:15 +0800

    添加react

--- docs/.vitepress/loadRoute.js
+++ docs/.vitepress/loadRoute.js
@@ -6,11 +6,11 @@ const excludeFileReg = /^\.|test|node_modules|\.(jpg|png|js|css)$/i
 export function readRouteMap(prevPath, prev = '', list = []) {
   const result = readdirSync(prevPath, { withFileTypes: true })
   for (let i = 0; i < result.length; i++) {
-    const dirent = result[i];
-    if (dirent.name.match(excludeFileReg)) continue
+    const direct = result[i];
+    if (direct.name.match(excludeFileReg)) continue
     
-    const {item: dirItem, itemLink} = generateItem(dirent, prev, dirent.isDirectory())
-    if (dirent.isDirectory()) {
+    const {item: dirItem, itemLink} = generateItem(direct, prev, direct.isDirectory())
+    if (direct.isDirectory()) {
       const dirPath = path.resolve(prevPath, dirItem.text)
       readRouteMap(dirPath, itemLink, dirItem.items)
       list.push(dirItem)
